2a2b2547980f6dfa2941ecec9146e2b22ba7e747
eregiona <eregiona@mail.ru>
2021-11-06 14:30:39 +0300

    end scroll

--- js/burger-anim/animation.js
+++ js/burger-anim/animation.js
@@ -5,7 +5,7 @@ document.querySelector('.burger').addEventListener('click', function(){
 })
 
 //making button in menu alive
-document.querySelector('.button').addEventListener('click', function(){
+document.querySelector('.burger_button').addEventListener('click', function(){
   document.querySelector('.burger span.active').classList.remove('active');
   document.querySelector('.menu.animate').classList.remove('animate');
 })
@@ -31,5 +31,45 @@ $(function() {
   });
  });
 
+// scrolling within burger__menu
+const burgerLinks = document.querySelectorAll('.burger__link[data-goto]');
+if (burgerLinks.length > 0) {
+  burgerLinks.forEach(burgerLink => {
+    burgerLink.addEventListener("click", onBurgerLinkClick);
+  }); 
 
+  function onBurgerLinkClick(e) {
+    const burgerLink = e.target;
+    if (burgerLink.dataset.goto && document.querySelector(burgerLink.dataset.goto)){
+      const gotoBlock = document.querySelector(burgerLink.dataset.goto);
+      const gotoBlockValue = gotoBlock.getBoundingClientRect().top + window.pageYOffset - document.querySelector('header').offsetHeight;
 
+      window.scrollTo({
+        top: gotoBlockValue,
+        behavior: "smooth"
+      });
+      e.preventDefault();
+    }
+  }
+}    
+// scrolling without burger_menu
+const toolsLinks = document.querySelectorAll('.tools__link[data-goto]');
+if (toolsLinks.length > 0) {
+  toolsLinks.forEach(toolsLink => {
+    toolsLink.addEventListener("click", onToolsLinkClick);
+  }); 
+
+  function onToolsLinkClick(e) {
+    const toolsLink = e.target;
+    if (toolsLink.dataset.goto && document.querySelector(toolsLink.dataset.goto)){
+      const gotoBlock = document.querySelector(toolsLink.dataset.goto);
+      const gotoBlockValue = gotoBlock.getBoundingClientRect().top + window.pageYOffset - document.querySelector('header').offsetHeight;
+
+      window.scrollTo({
+        top: gotoBlockValue,
+        behavior: "smooth"
+      });
+      e.preventDefault();
+    }
+  }
+}   
